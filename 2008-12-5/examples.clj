cd1e93a25efdc3beec0cc7622c41bc658b0f09c7
Ian Dees <undees@gmail.com>
2008-12-05 11:18:30 -0800

    Added BEQ, BUQ

--- 2008-12-5/examples.clj
+++ 2008-12-5/examples.clj
@@ -26,3 +26,30 @@
 (println (format "0 and 1 = %d" (logical-and 0 1)))
 (println (format "1 and 0 = %d" (logical-and 1 0)))
 (println (format "1 and 1 = %d" (logical-and 1 1)))
+
+(defn exists [pred]
+  (p-r z (o logical-or (o pred (p 2 1)) (p 2 2))))
+
+(defn for-all [pred]
+  (p-r (o s z) (o logical-and (o pred (p 2 1)) (p 2 2))))
+
+(def negative z) ; false for all n in N, by inspection
+
+(def logical-not (o if-then-else (p 1 1) z (o s z)))
+
+(def non-negative (o logical-not negative))
+
+(def even (p-r (o s z) (o logical-not (p 2 2))))
+
+(println (format "Some x < 10 is negative? %d"
+         ((exists negative) 10)))
+(println (format "All x < 10 are negative? %d"
+         ((for-all negative) 10)))
+(println (format "Some x < 10 is non-negative? %d"
+         ((exists non-negative) 10)))
+(println (format "All x < 10 are non-negative? %d"
+         ((for-all non-negative) 10)))
+(println (format "Some x < 10 is even? %d"
+         ((exists even) 10)))
+(println (format "All x < 10 are even? %d"
+         ((for-all even) 10)))
