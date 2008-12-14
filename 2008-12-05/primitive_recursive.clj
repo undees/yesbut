540903a1427818d462755925b0293c92077cbb9a
Ian Dees <undees@gmail.com>
2008-12-14 12:09:46 -0800

    Added P-R docstrings

--- 2008-12-05/primitive_recursive.clj
+++ 2008-12-05/primitive_recursive.clj
@@ -1,27 +1,46 @@
 (ns primitive-recursive)
 
-(defn- args [arity]
+(defn- args
+  "Returns an argument list of the given arity."
+  [arity]
   (map (fn [n] (symbol (str \p n)))
        (range 0 arity)))
 
-(defmacro z [arity]
+(defmacro z
+  "Yields a function of the given arity
+  that always returns zero."
+  [arity]
   (let [xs (args arity)]
     `(fn [~@xs] 0)))
 
-(def s inc)
+(def
+  #^{:doc "Successor function; increments
+  its argument by one."}
+  s inc)
 
-(defmacro p [super sub]
+(defmacro p
+  "Yields a function taking a number of
+  arguments (superscript) and returning
+  the one given by the subscript."
+  [super sub]
   (let [xs (args super)]
     `(fn [~@xs] ~(nth xs (dec sub)))))
 
-(defn o [f & gs]
+(defn o
+  "Returns a function composing a function f
+  and one or more functions gs."
+  [f & gs]
   (fn [& xs]
     (apply f
            (map
              (fn [g] (apply g xs))
              gs))))
 
-(defn p-r [g h]
+(defn p-r
+  "Returns a function defined by primitive
+  recursion on a base-case step g and a
+  recursive step h."
+  [g h]
   (fn f [x & ys]
     (if (zero? x)
         (apply g ys)
